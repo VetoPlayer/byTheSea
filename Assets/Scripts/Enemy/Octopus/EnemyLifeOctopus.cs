7ce7b50c3a1798ecd4ef10a05bae5498a1a27c32
Simone Deola <simonedeola@eduroam-181-184.uninsubria.it>
2016-11-25 16:37:06 +0100

    Nemici differenziati

---
+++ Assets/Scripts/Enemy/Octopus/EnemyLifeOctopus.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLifeOctopus : MonoBehaviour {
+
+
+	public int initialLife = 100;
+
+	int currentLife=100;
+
+
+	// Use this for initialization
+	void Start () {
+		currentLife = initialLife;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool decreaseLife(int attack){
+		
+		currentLife = currentLife - gameObject.GetComponent<ShieldResponseOctopus>().response( attack );
+		if (currentLife <= 0) {
+			//death procedure
+			death ();
+			return true;
+		} else {
+
+			return false;
+		}
+	}
+
+	public void death (){
+
+		//CHANGE THIS, OR NOT
+		this.gameObject.SetActive (false);
+
+		//CALL SOMETHING??
+	}
+
+}
---
+++ Assets/Scripts/Enemy/Octopus/ShieldResponseOctopus.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldResponseOctopus : MonoBehaviour {
+
+	public int numberOfShield=4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int response(int attack){
+		decreaseShield (attack);
+		if (numberOfShield < 0)
+			return attack;
+		else
+			return 0;
+	}
+
+	public void decreaseShield(int attack){
+		if (numberOfShield > 0) {
+			numberOfShield = numberOfShield - 1;
+			if (numberOfShield < 0) {
+				//LEVA L'ATTACCO
+				gameObject.GetComponent<BulletResponse> ().escapeRate = -0.1f;
+			} else {
+				//CONTRATTACCO?????
+			}
+		}
+	}
+}

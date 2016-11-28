7bd56222fda46bc8fd2860b5bc50751985dc4218
Simone Deola <simonedeola@MacBook-Air-di-Simone-2.local>
2016-11-28 16:44:49 +0100

    endig octopus

---
+++ Assets/InkBulletMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InkBulletMovement : MonoBehaviour {
+
+
+	public float speed=10;
+	public int attack=10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = new Vector3 (-1, 0, 0); //here we can change position if we wanto to make it "round-shaped"
+		transform.position = transform.position + dir * speed * Time.deltaTime;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "Castle") {
+			other.GetComponent<CastleLife> ().decreaseLife (attack);
+			gameObject.SetActive (false);
+		}
+
+
+	}
+
+
+
+}
---
+++ Assets/RangedAttack.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedAttack : MonoBehaviour {
+
+	private float lastShot=0.0f;
+	public GameObject inkBullet;
+	public GameObject inkFireSpot;
+	public float timeRange=1;
+
+	// Use this for initialization
+	void Start () {
+		lastShot = Time.time;
+		ObjectPoolingManager.Instance.CreatePool (inkBullet, 100, 200);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (((Time.time - lastShot) >= timeRange) && fire ()) {
+
+
+
+			lastShot = Time.time;
+
+			GameObject b=ObjectPoolingManager.Instance.GetObject (inkBullet.name);
+			b.transform.position = inkFireSpot.transform.position;
+			b.transform.rotation = inkFireSpot.transform.rotation;
+
+		}
+
+
+	
+	}
+
+	public bool fire(){
+
+		LayerMask CastleLayer = 1 << LayerMask.NameToLayer("Castle");
+
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.left, Mathf.Infinity, CastleLayer);
+		if (hit.collider!=null) {
+			return true;
+
+		}
+		return false;
+
+	}
+}

350da6ce26a3d7779e78ebb0dd9895dbb76ca482
Andrew <wormxexraped@gmail.com>
2016-11-30 15:20:15 +0100

    Added Tile Spawing Towers

--- Assets/Scripts/Grid/Tile.cs
+++ Assets/Scripts/Grid/Tile.cs
@@ -8,18 +8,54 @@ public class Tile : MonoBehaviour {
 
 	private bool free=true;
 
+	private bool displaying_in_prevew=false;
+
+	[Header("Water Resource to be spawn over the tile")]
 	public GameObject m_my_tile_water;
+
+	[Header("Archer Castle")]
+	public GameObject m_archer_castle_prefab;
+
+	[Header("Cannon Castle")]
+	public GameObject m_cannon_castle_prefab;
+
+	[Header("Base Castle")]
+	public GameObject m_base_castle_prefab;
+
+
+	private bool creative_mode = false;
+
+	private BuildableEnum castle_to_build =  BuildableEnum.NoBuilding;
+
+	// Tower instance that is being previewed
+	private GameObject tower_preview;
+
+	//Tower insance that has been built so far
+	private GameObject tower_built;
+
 
 	// Use this for initialization
 	void Start () {
 		//The tile takes track of its own position such that it'll spawn buildings over itself
 		tr = GetComponent<Transform> (); 
 
-
-
-		// I don't like IT! It seems it's the only way
+		//WATER
 		ObjectPoolingManager.Instance.CreatePool (m_my_tile_water, 50,50);
-	
+
+		// ArcherCastle
+
+		ObjectPoolingManager.Instance.CreatePool ( m_archer_castle_prefab, 50,50);
+
+
+		EventManager.StartListening ("ArcherCastle",setArcherCastle);
+		EventManager.StartListening ("CannonCastle",setCannonCastle);
+		EventManager.StartListening ("BaseCastle",setBaseCastle);
+
+		//TESTS By ME!!
+
+
+		EventManager.TriggerEvent ("ArcherCastle");
+
 	}
 	
 	// Update is called once per frame
@@ -29,29 +65,42 @@ public class Tile : MonoBehaviour {
 
 	// Here The Single Tile has to spawn an image over itself (to be setted s its son)
 	void OnMouseOver(){
-		Debug.Log ("Sei sopra di me");
-		// The tile accesses to the global variable to see if there is a building
-		// BuildableEnum building_to_build = GlobalVariables.instance.GetBuilding();
-
-
-
+		if (displaying_in_prevew == false && free == true && creative_mode == true && castle_to_build != BuildableEnum.NoBuilding) {
+			string castle_name = castle_to_build.ToString ();
+			GameObject go = ObjectPoolingManager.Instance.GetObject (castle_name);
+			go.transform.position = tr.transform.position;
+			go.transform.rotation = Quaternion.identity;
+			tower_preview = go;
+			displaying_in_prevew = true;
+		}
+			
 	}
 
 	// Here The Single Tile has to make the image over itself disappear
 	void OnMouseExit(){
-		Debug.Log ("Sei uscito!");
+		if (displaying_in_prevew == true && free == true) {
+			tower_preview.SetActive (false);
+			displaying_in_prevew = false;
+		}
+			
+	}
+
+
+	void OnMouseUp(){
+		tower_built = tower_preview;
+		free = false;
 
 	}
 
 	// This methodsets water over the selected tile.
 	public void SetWater(){
 		//It should instantiate a water sprite!
-
+	
 		free = false;
-		GameObject go = ObjectPoolingManager.Instance.GetObject("Water");
+		GameObject go = ObjectPoolingManager.Instance.GetObject(m_my_tile_water.name);
 		go.transform.position = tr.position;
 		go.transform.rotation = Quaternion.identity;
-		Debug.Log("Water setted?! Lo spero!");
+
 	
 	
 	}
@@ -61,4 +110,21 @@ public class Tile : MonoBehaviour {
 		return;
 	}
 
+
+	private void setBaseCastle(){
+		//TODO
+	}
+
+
+	private void setArcherCastle(){
+		creative_mode = true;
+		castle_to_build = BuildableEnum.ArcherTower;
+	}
+
+
+	private void setCannonCastle(){
+		creative_mode = true;
+		castle_to_build = BuildableEnum.CannonTower;
+	}
+		
 }
--- Assets/Scripts/Resources/ResourceButton.cs
+++ Assets/Scripts/Resources/ResourceButton.cs
@@ -14,18 +14,48 @@ public class ResourceButton : MonoBehaviour {
 	/// </summary>
 	public ResourcesEnum m_resource;
 
+	[Header("Inactive Time"), Range(0f,3f), Tooltip("Time to wait to have the button active after a click")]
+	/// <summary>
+	/// Time you have to wait to have the button active again after a click
+	/// </summary>
+	public float m_inactiveTime = 1f;
 
+	[Header("Button")]
+	/// <summary>
+	/// Button to assign to this script.
+	/// </summary>
+	[Tooltip("Button Object that have to be assigned to this script")]
+	public Button m_button;
+	/// <summary>
+	/// Text of the Button assigned to this script
+	/// </summary>
+	[Tooltip("Text of the Button Object assigned to this script")]
+	public Text m_button_text;
 
 	// l'ultima volta che è stato cliccato il bottone
 	private float lastSpawnTime;
 
-	void Awake(){}
+	void Awake(){
+		this.m_button = GetComponent<Button>() as Button;
+	}
 
 	// Use this for initialization
-	void Start () {}
+	void Start () {
+		// to have the initial label = "Spawn <resource>"
+		this.lastSpawnTime = Time.time-this.m_inactiveTime;
+	}
 
 	// Update is called once per frame
-	void Update () {}
+	void Update () {
+		
+		if (Time.time - this.lastSpawnTime > m_inactiveTime) {
+			this.m_button.interactable = true;
+			this.m_button_text.text = "Spawn " + m_resource.ToString ();
+		} else {
+			this.m_button_text.text = "Wait: " 
+				+ Mathf.FloorToInt (m_inactiveTime - (Time.time - this.lastSpawnTime) + 1).ToString ();
+		}
+	}
 
 	/// <summary>
 	/// Temporary disable the assigned button and trigger the spawn event of the assigned resource.
@@ -33,10 +63,6 @@ public class ResourceButton : MonoBehaviour {
 	public void spawnResource(){
 		this.lastSpawnTime = Time.time;
 		this.m_resource.fireSpawnEvent ();
-	}
-
-	void OnMouseDown(){
-		this.spawnResource ();
-		this.gameObject.SetActive (false);
+		this.m_button.interactable = false;
 	}
 }

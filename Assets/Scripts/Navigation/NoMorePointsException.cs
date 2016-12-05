016479bde4087e1956654e31a368ea66b678b3e7
davide_cremona <davide_cremona@10.169.151.230>
2016-12-05 12:21:21 +0100

    platform part started

---
+++ Assets/Scripts/Navigation/NoMorePointsException.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NoMorePointsException : Exception {
+
+	public NoMorePointsException(string message) : base(message){
+	}
+}
---
+++ Assets/Scripts/Navigation/Point.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Point : MonoBehaviour {
+
+	[Header("Next Point in the Path")]
+	public List<Transform> m_nextPoints;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	public Vector3 getNextPoint(){
+		print ("I have: " + (this.m_nextPoints.Count).ToString());
+		if (this.m_nextPoints.Count != 0) {
+			int element = Random.Range (0, this.m_nextPoints.Count - 1);
+			return m_nextPoints [element].position;
+		} else {
+			throw new NoMorePointsException ("no more points");
+		}
+	}
+}

82d5007a14101c2035e0fb6a3346382475818690
davide_cremona <davide_cremona@10.169.151.230>
2016-12-06 11:56:35 +0100

    platform player

---
+++ Assets/Scripts/Navigation/PlayerMovements.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovements : MonoBehaviour {
+
+	[Header("Player Parameters")]
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

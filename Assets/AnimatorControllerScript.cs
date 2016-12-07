ba86ae26259d0b25239f1b8da21e35a5f6525431
Simone Deola <simonedeola@Air-di-Simone-2.homenet.telecomitalia.it>
2016-12-07 16:44:22 +0100

    secchiello

---
+++ Assets/AnimatorControllerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorControllerScript : MonoBehaviour {
+
+	Animator animator;
+	public float TimeOfAnimation;
+	// Use this for initialization
+	void Start () {
+
+		animator = GetComponent<Animator> () as Animator;
+		animator.speed = calculateSpeed ( TimeOfAnimation );
+		animator.SetTrigger ("Rotate");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private float calculateSpeed(float wantedTime){
+		return 0.29f / wantedTime ;
+	}
+
+}

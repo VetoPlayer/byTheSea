c7eacfc25a9e7f19a0fe8a337d46b07ba0c87715
davide_cremona <davide_cremona@10.169.151.230>
2016-12-12 19:11:45 +0100

    pre-build commit

---
+++ Assets/Scripts/Enemy/EnemyStatesChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStatesChanger : MonoBehaviour {
+
+	private bool attackState;
+	private bool movementState;
+
+	// Use this for initialization
+	void Start () {}
+
+	void OnEnable(){
+		this.gameObject.GetComponent<EnemyAttack> ().enabled = false;
+		this.gameObject.GetComponent<EnemyMovement> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+}

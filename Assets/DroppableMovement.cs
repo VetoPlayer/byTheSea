c40fbf5f8e86f3c17416f68911bb59e13596bb8b
Simone Deola <simonedeola@eduroam-181-131.uninsubria.it>
2017-01-02 15:37:48 +0100

    acqua e sabbia si muovono un po, cosi il giocatore capisce che deve raccoglierli

---
+++ Assets/DroppableMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroppableMovement : MonoBehaviour {
+	float speed = 0.3f;
+	int i=0;
+	Vector3 dir;
+	// Use this for initialization
+	void Start () {
+		dir = new Vector3 (0, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i == 30) {
+			dir = -dir;
+			i = 0;
+		}
+		//here we can change position if we wanto to make it "round-shaped"
+		transform.position = transform.position + dir * speed * Time.deltaTime;
+		i++;
+
+	}
+}

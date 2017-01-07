4d6e8704ebaf583f102355f045670a066c0bfdb3
davide_cremona <davide_cremona@10.169.151.230>
2017-01-07 19:00:37 +0100

    cell collider resize

--- Assets/TileGraphic.cs
+++ Assets/TileGraphic.cs
@@ -9,23 +9,47 @@ public class TileGraphic : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		EventManager.StartListening ("GridResized", resizeCollider);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void resizeCollider(){
+		StartCoroutine (resize ());
+	}
+
+	IEnumerator resize(){
+		yield return new WaitForEndOfFrame();
+
+		/*
 		// resizing dimension --> to verify
 		GameObject parent = this.gameObject.transform.parent.gameObject;
 		RectTransform prt = parent.GetComponent<RectTransform> () as RectTransform;
 		Vector2 size = new Vector2 (prt.rect.width - m_cellPadding, prt.rect.height - m_cellPadding);
 		RectTransform rt = this.GetComponent<RectTransform> () as RectTransform;
 		rt.rect.Set (size.x, size.y, size.x, size.y);
+		*/
 
+		RectTransform parentRT = this.gameObject.GetComponentInParent<RectTransform> () as RectTransform;
 
+		Rect parentRect = parentRT.rect;
+
+		Vector2 colliderSize = this.gameObject.GetComponent<BoxCollider2D> ().size;
+		colliderSize.x = parentRect.width * 1.55f;
+		colliderSize.y = parentRect.height * 1.55f;
+
+		this.gameObject.GetComponent<BoxCollider2D> ().size = colliderSize;
+
+		/*
 		// resizing collider
 		Vector2 colliderSize = this.gameObject.GetComponent<BoxCollider2D>().size;
 		colliderSize.x = size.x*0.35f;
 		colliderSize.y = size.y*0.35f;
 		this.gameObject.GetComponent<BoxCollider2D> ().size = colliderSize;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
+		*/
 	}
 }
---
+++ Assets/TimerResize.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerResize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		EventManager.StartListening ("GridResized", resizeTimer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void resizeTimer(){
+
+		StartCoroutine (resize ());
+	}
+
+	IEnumerator resize(){
+		yield return new WaitForEndOfFrame();
+
+		RectTransform localRT = this.gameObject.GetComponent<RectTransform> () as RectTransform;
+		RectTransform parentRT = this.gameObject.GetComponentInParent<RectTransform> () as RectTransform;
+
+		Vector2 localSize = new Vector2 (localRT.sizeDelta.x, localRT.sizeDelta.y);
+		Vector2 parentSize = new Vector2 (parentRT.sizeDelta.x, localRT.sizeDelta.y);
+
+		localSize.x = parentSize.x;
+		localSize.y = parentSize.y;
+
+		localRT.sizeDelta = parentSize;
+	}	
+}
